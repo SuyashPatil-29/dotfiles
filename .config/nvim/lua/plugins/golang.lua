468ea499f2a2f97656e8ac732e604e65d1f21a84
SuyashPatil-29 <patilsuyash892@gmail.com>
2024-07-02 10:28:24 +0530

    Added lsp support for golang

--- .config/nvim/lua/plugins/golang.lua
+++ .config/nvim/lua/plugins/golang.lua
@@ -1,64 +1,49 @@
-return {
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
   {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
-
-      "leoluz/nvim-dap-go",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      vim.notify = require "notify"
-
-      require("go").setup {
-        capabilities = capabilities,
-        -- lsp_on_attach = require("plugins.lsp.on_attach").on_attach,
-        lsp_cfg = {
-          settings = {
-            gopls = {
-              gofumpt = true,
-              codelenses = {
-                gc_details = false,
-                generate = true,
-                regenerate_cgo = true,
-                run_govulncheck = true,
-                test = true,
-                tidy = true,
-                upgrade_dependency = true,
-                vendor = true,
-              },
-              hints = {
-                assignVariableTypes = false,
-                compositeLiteralFields = false,
-                compositeLiteralTypes = false,
-                constantValues = false,
-                functionTypeParameters = false,
-                parameterNames = false,
-                rangeVariableTypes = false,
-              },
-              analyses = {
-                fieldalignment = true,
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-                useany = true,
-              },
-              usePlaceholders = true,
-              completeUnimported = true,
-              staticcheck = true,
-              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-              semanticTokens = false,
-            },
-          },
-        },
-        luasnip = true,
-        trouble = true,
-      }
+      require("go").setup()
     end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()',
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.lsp.golang.go-lsp-config"
+      require "plugins.lsp-config"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "plugins.lsp.golang.go-null-ls-config"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
   },
 }
+return plugins

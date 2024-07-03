9e6ac5eec3901207ff6d308666516baf5f961b06
SuyashPatil-29 <patilsuyash892@gmail.com>
2024-07-03 18:07:19 +0530

    Rewrote better lsp config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -8,9 +8,31 @@ return {
     { "b0o/schemastore.nvim" },
     "folke/neodev.nvim",
     "OmniSharp/omnisharp-vim",
+    "rcarriga/nvim-notify",
   },
   lazy = false,
   config = function()
+    -- Set up nvim-notify
+    vim.notify = require "notify"
+
+    -- Table to store start times for each LSP server
+    local lsp_start_times = {}
+
+    -- Flag to check if a start notification has been shown
+    local start_notification_shown = false
+
+    -- Function to show LSP loading notification
+    local function lsp_notify(msg, level)
+      vim.notify(msg, level, {
+        title = "LSP",
+        timeout = 500,
+        keep = function()
+          return vim.bo.filetype == "lspinfo"
+        end,
+      })
+    end
+
+    -- Mason setup
     require("mason").setup {
       ui = {
         border = "rounded",
@@ -22,9 +44,7 @@ return {
       },
     }
 
-    local util = require "lspconfig/util"
-    local configs = require "lspconfig/configs"
-
+    -- Mason-lspconfig setup
     require("mason-lspconfig").setup {
       ensure_installed = vim.tbl_keys(require "plugins.lsp.servers"),
     }
@@ -33,11 +53,62 @@ return {
       ensure_installed = { "tailwindcss", "html", "clangd" },
     }
 
-    require("lspconfig").clangd.setup {
-      cmd = { "clangd", "--client-encoding=utf-8" },
-    }
+    -- Set up neodev before setting up other LSPs
+    require("neodev").setup()
 
-    require("lspconfig").tsserver.setup {
+    -- LSP configurations
+    local lspconfig = require "lspconfig"
+    local util = require "lspconfig/util"
+
+    -- Capabilities
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+    -- On attach function
+    local on_attach = function(client, bufnr)
+      local start_time = lsp_start_times[client.name]
+      if start_time then
+        local load_time = vim.loop.hrtime() - start_time
+        local load_time_ms = load_time / 1e6 -- Convert nanoseconds to milliseconds
+        lsp_notify(string.format("%s LSP loaded in %.2f ms", client.name, load_time_ms), "info")
+      else
+        lsp_notify(client.name .. " LSP loaded", "info")
+      end
+      -- Your existing on_attach logic here
+    end
+
+    -- Function to set up LSP servers
+    local function setup_lsp(server_name, config)
+      config = config or {}
+      config.on_attach = on_attach
+      config.capabilities = capabilities
+
+      -- Show start notification if not already shown
+      if not start_notification_shown then
+        lsp_notify("Starting LSP servers...", "info")
+        start_notification_shown = true
+      end
+
+      lsp_start_times[server_name] = vim.loop.hrtime()
+
+      lspconfig[server_name].setup(config)
+    end
+
+    -- Clangd setup
+    setup_lsp("clangd", {
+      filetypes = { "c", "cpp", "objc", "objcpp" },
+    })
+
+    -- TypeScript setup
+    setup_lsp("tsserver", {
+      filetypes = {
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+      },
       settings = {
         typescript = {
           inlayHints = {
@@ -60,69 +131,55 @@ return {
           },
         },
       },
-    }
+    })
 
-    require("lspconfig").gopls.setup {
+    -- Gopls setup
+    setup_lsp("gopls", {
       cmd = { "gopls" },
       filetypes = { "go", "gomod", "gowork", "gotmpl" },
-      root_dir = util.root_pattern("go.mod", "go.work", "git"),
+      root_dir = util.root_pattern("go.work", "go.mod", ".git"),
       settings = {
         gopls = {
-          compeleteUnimported = true,
+          completeUnimported = true,
           usePlaceholders = true,
           analyses = {
             unusedparams = true,
-            unreachable = true,
-            unusedvars = true,
-            ineffassign = true,
-            nilerr = true,
-            misspell = true,
-            typecheck = true,
-            errcheck = true,
-            staticcheck = true,
-            gosimple = true,
-            scopelint = true,
           },
         },
       },
-    }
+    })
 
-    require("lspconfig.ui.windows").default_options.border = "single"
-
-    require("neodev").setup()
-
-    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
-    -- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-
-    local mason_lspconfig = require "mason-lspconfig"
-
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require("lspconfig")[server_name].setup {
-          capabilities = capabilities,
-          on_attach = require("plugins.lsp.on_attach").on_attach,
-          settings = require("plugins.lsp.servers")[server_name],
-          filetypes = (require("plugins.lsp.servers")[server_name] or {}).filetypes,
-        }
-      end,
-    }
-
-    -- Python environment
-    local path = util.path
-    require("lspconfig").pyright.setup {
-      on_attach = require("plugins.lsp.on_attach").on_attach,
-      capabilities = capabilities,
+    -- Python setup
+    setup_lsp("pyright", {
       before_init = function(_, config)
-        -- pwd path where all python codes should be stored for python to work properly
+        local path = util.path
         local default_venv_path = path.join(vim.env.HOME, "Desktop", "NHCE", "Sem4", "ds", "bin", "python")
         local default_venv_path_for_DAA = path.join(vim.env.HOME, "Desktop", "NHCE", "Sem4", "daa", "bin", "python")
         config.settings.python.pythonPath = default_venv_path
         config.settings.python.pythonPath = default_venv_path_for_DAA
       end,
+      filetypes = { "python" },
+    })
+
+    -- Mason-lspconfig handler for other servers
+    local mason_lspconfig = require "mason-lspconfig"
+    mason_lspconfig.setup_handlers {
+      function(server_name)
+        if
+          server_name ~= "gopls"
+          and server_name ~= "clangd"
+          and server_name ~= "tsserver"
+          and server_name ~= "pyright"
+        then
+          setup_lsp(server_name, {
+            settings = require("plugins.lsp.servers")[server_name],
+            filetypes = (require("plugins.lsp.servers")[server_name] or {}).filetypes,
+          })
+        end
+      end,
     }
 
+    -- Diagnostic configuration
     vim.diagnostic.config {
       title = false,
       underline = true,
@@ -139,7 +196,8 @@ return {
       },
     }
 
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+    -- Diagnostic signs
+    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
     for type, icon in pairs(signs) do
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,18 +1,6 @@
--- return {
---   "nvimtools/none-ls.nvim", -- The plugin name
---   config = function()
---     local null_ls = require("null-ls") -- Require the null-ls plugin
---
---     null_ls.setup({
---       sources = {
---         null_ls.builtins.formatting.stylua, -- Use the built-in stylua formatter
---         null_ls.builtins.formatting.prettier, -- Use the built-in prettier formatter
---         null_ls.builtins.completion.spell, -- Use the built-in spell completion
---       },
---     })
---
---   end,
--- }
+local null_ls = require "null-ls"
+local null_ls_utils = require "null-ls.utils"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 return {
   {
@@ -29,22 +17,17 @@ return {
       require("null-ls").register(require "none-ls-shellcheck.code_actions")
 
       local mason_null_ls = require "mason-null-ls"
-      local null_ls = require "null-ls"
-
-      local null_ls_utils = require "null-ls.utils"
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {}) -- Create an autocmd group for formatting
-
       mason_null_ls.setup {
         ensure_installed = {
-          "prettier", -- prettier formatter
-          "stylua", -- lua formatter
-          "eslint_d", -- js linter
-          "golangci_lint", -- go linter
-          "shellcheck", -- shell linter
-          "buf", -- buf formatter
-          "gofumpt", -- go formatter
-          "spell", -- spell checker
-          "black", -- python formatter
+          "prettier",
+          "stylua",
+          "eslint_d",
+          "golangci_lint",
+          "shellcheck",
+          "buf",
+          "gofumpt",
+          "spell",
+          "black",
         },
       }
 
@@ -52,58 +35,45 @@ return {
       local diagnostics = null_ls.builtins.diagnostics
       local code_actions = null_ls.builtins.code_actions
 
-      -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local sources = {
+        -- Formatting
+        formatting.stylua,
+        formatting.prettier,
+        formatting.gofumpt,
+        formatting.goimports_reviser,
+        formatting.golines,
+        formatting.buf,
+        formatting.black,
+
+        -- Diagnostics
+        diagnostics.golangci_lint,
+
+        -- Code Actions
+        code_actions.gitsigns,
+        code_actions.refactoring,
+      }
+
+      local on_attach = function(client, bufnr)
+        if client.supports_method "textDocument/formatting" then
+          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format { bufnr = bufnr }
+            end,
+          })
+        end
+      end
 
       null_ls.setup {
         root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
-
-        sources = {
-          formatting.stylua,
-          formatting.prettier,
-          formatting.gofumpt,
-          formatting.goimports_reviser,
-          formatting.golines,
-          formatting.buf,
-          formatting.black,
-
-          diagnostics.golangci_lint,
-
-          code_actions.gitsigns,
-          code_actions.refactoring,
-        },
-        on_attach = function(client, bufnr)
-          if client.supports_method "textDocument/formatting" then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr } -- Clear existing autocmds for the buffer
-            vim.api.nvim_create_autocmd("BufWritePre", { -- Create a new autocmd that runs before saving the buffer
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format { bufnr = bufnr } -- Format the buffer
-              end,
-            })
-          end
-        end,
-        -- configure format on save
-        -- on_attach = function(current_client, bufnr)
-        --   if current_client.supports_method("textDocument/formatting") then
-        --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        --     vim.api.nvim_create_autocmd("BufWritePre", {
-        --       group = augroup,
-        --       buffer = bufnr,
-        --       callback = function()
-        --         vim.lsp.buf.format({
-        --           filter = function(client)
-        --             --  only use null-ls for formatting instead of lsp server
-        --             return client.name == "null-ls"
-        --           end,
-        --           bufnr = bufnr,
-        --         })
-        --       end,
-        --     })
-        --   end
-        -- end,
+        sources = sources,
+        on_attach = on_attach,
       }
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" }) -- Keybinding to format the buffer manually
+
+      -- Keybinding to format the buffer manually
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format buffer" })
     end,
   },
 }

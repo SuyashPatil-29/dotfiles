f8dd9e455ea233f18c25953139629e6b4b5fc4ff
SuyashPatil-29 <patilsuyash892@gmail.com>
2024-07-19 23:26:16 +0530

    WIP

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -249,7 +249,7 @@ end
 local function setup_key_mappings()
   local builtin = require "telescope.builtin"
   vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-  vim.keymap.set("n", "<leader>lg", builtin.live_grep, { desc = "Open live grep" })
+  vim.keymap.set("n", "<leader>ff", builtin.live_grep, { desc = "Open live grep" })
 end
 
 return {
